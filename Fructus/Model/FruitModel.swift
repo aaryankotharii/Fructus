7198c86bfdeafeb4bc287100089f81676eb58943
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-12 19:22:48 +0530

    added: Fruit model

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,22 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Aaryan Kothari on 12/09/20.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit : Identifiable  {
+    var id = UUID()
+    var title : String
+    var headline : String
+    var image : String
+    var gradientColors : [Color]
+    var description : String
+    var nutrition : [String]
+}
+
+
