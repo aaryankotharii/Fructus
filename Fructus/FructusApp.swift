e699561676ba49ff1331d8420a49408ae13568f1
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-12 12:56:41 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aaryan Kothari on 12/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
