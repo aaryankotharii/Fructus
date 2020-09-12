b132aeca56bdfd8cfc0f4de8db4b5ce6793d2d53
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-12 19:09:25 +0530

    Onboarding basic setup complete

---
+++ Fructus/New Group/OnboardingView.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Aaryan Kothari on 12/09/20.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK - PROPERTIES
+    
+    //MARK - BODY
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { fruitCard in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical,20)
+    }
+}
+
+//MARK - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
