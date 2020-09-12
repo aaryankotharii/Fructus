5a15e4ac19fc92e66eb0fc3438aa9c4e2b8a5f7b
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-09-12 21:00:48 +0530

    ContentView setup complete ✅ (#3)
    * added: FruitRowView
    
    * added: fruits list in ContentView

--- Fructus/New Group/ContentView.swift
+++ Fructus/New Group/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    var fruits : [Fruit] = fruitsData
+    
+    // MARK - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List{
+                ForEach(fruits.shuffled()) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical,4)
+                }
+            } //: LIST
+            .navigationTitle("FRUITS")
+    } //: NAVIGATION
     }
 }
 
+// MARK - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
