529e08016d09fadd5b6cd4e5ba4ec68effe17b49
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-12 18:51:36 +0530

    created: StartButtonView

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Aaryan Kothari on 12/09/20.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    //MARK - PROPERTIES
+    
+    //MARK - BODY
+    var body: some View {
+        Button(action: startClicked, label: {
+            HStack(spacing: 8){
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16.0)
+            .padding(.vertical, 10.0)
+            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.25))
+
+        }) //: BUTTON
+    }
+    
+    func startClicked(){
+        
+    }
+}
+
+//MARK - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+    }
+}
