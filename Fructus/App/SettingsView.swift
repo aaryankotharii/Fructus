e6e00f67c61d63b75d9ac215e491f8fbd764c1f5
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-13 11:27:39 +0530

    added: settings customisation view

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
     // MARK - PROPERTIES
     @Environment(\.presentationMode) var presentztionMode
+    @AppStorage("inOnboarding") var isOnboarding :Bool = false
     
     // MARK - BODY
     var body: some View {
@@ -36,6 +37,20 @@ struct SettingsView: View {
                     }
                     
                     // MARK - SECTION 2
+                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")){
+                        Divider().padding(.vertical,4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                          .padding(.vertical, 8)
+                          .frame(minHeight: 60)
+                          .layoutPriority(1)
+                          .font(.footnote)
+                          .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding){
+                            Text("RESTART")
+                        }
+                    }
                     
                     // MARK - SECTION 3
                     GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")){
@@ -45,7 +60,7 @@ struct SettingsView: View {
                         SettingsRowView(name: "Linkedin", linklabel: "@aaryankotharii",linkDestination: "www.linkedin.com/in/aaryankotharii/")
                         SettingsRowView(name: "SwiftUI", content: "2.0")
                         SettingsRowView(name: "Version", content: "1.1.0")
-                    }   
+                    }
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"),displayMode: .large)
                 .navigationBarItems(trailing: crossButton)
