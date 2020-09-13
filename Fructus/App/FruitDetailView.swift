bd3605c05742a9162d2910dff2809a74d9b8c8cc
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-13 09:49:15 +0530

    added FruitNutrientsView

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -17,6 +17,8 @@ struct FruitDetailView: View {
             ScrollView(.vertical){
                 VStack(alignment: .center, spacing: 20){
                     // HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
                     VStack(alignment: .leading, spacing: 20) {
                         // TITLE
                         Text(fruit.title)
@@ -42,13 +44,18 @@ struct FruitDetailView: View {
                             .multilineTextAlignment(.leading)
                         
                         // LINK
-                        
+                        SourceLinkView()
+                            .padding(.top,10)
+                            .padding(.bottom,40)
                         
                     } //: VSTACK
                     .padding(.horizontal,20)
                     .frame(maxWidth: 640,alignment: .center)
                 } //: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
             } //: SCROLL
+            .edgesIgnoringSafeArea(.top)
         } //: NAVIGATION
     }
 }
